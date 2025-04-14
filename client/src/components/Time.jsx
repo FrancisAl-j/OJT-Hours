dde009eaa0965f79a6098cf548113497e2ce2297
Francis <kikobilas123@gmail.com>
2025-04-14 22:52:13 +0800

    Update the UI on time/clock and added a new controller for updating the hours

--- client/src/components/Time.jsx
+++ client/src/components/Time.jsx
@@ -2,44 +2,58 @@ import React, { useEffect, useState } from "react";
 
 const Time = () => {
   const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(58);
+  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
 
   const getTime = () => {
     //const time = Date.now();
-
     //setHours(Math.floor((1000 * 60 * 60) % 24));
     //setMinutes(Math.floor((1000 / 60) % 60));
-    setSeconds((prevSec) => {
-      if (prevSec === 59) {
-        setMinutes((prevMins) => {
-          if (prevMins === 59) {
-            setHours((hours) => hours + 1);
-            return 0;
-          } else {
-            return prevMins + 1;
-          }
-        });
-        return 0;
-      } else {
-        return prevSec + 1;
-      }
-    });
   };
 
   useEffect(() => {
-    const interval = setInterval(() => getTime(), 1000);
+    let interval;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((prevSec) => {
+          if (prevSec === 59) {
+            setMinutes((prevMins) => {
+              if (prevMins === 59) {
+                setHours((hours) => hours + 1);
+                return 0;
+              } else {
+                return prevMins + 1;
+              }
+            });
+            return 0;
+          } else {
+            return prevSec + 1;
+          }
+        });
+      }, 1000);
+    }
 
     return () => clearInterval(interval);
-  }, []);
+  }, [isActive]);
 
   return (
-    <section className="h-[100svh]">
-      <div>
-        <div></div>
+    <section className="h-[100svh] grid place-items-center">
+      <div className="clock-container flex flex-col items-center gap-10 shadow-2xl shadow-black p-5">
+        <div className="flex items-center justify-between w-full">
+          <h1 className="text-white font-bold text-4xl">{hours} hr</h1>
+          <span className="text-white font-bold text-4xl">:</span>
+          <h1 className="text-white font-bold text-4xl">{minutes} mins</h1>
+          <span className="text-white font-bold text-4xl">:</span>
+          <h1 className="text-white font-bold text-4xl">{seconds} sec</h1>
+        </div>
 
-        <button onClick={() => setIsActive(true)}>Start</button>
+        <button
+          onClick={() => setIsActive(!isActive)}
+          className="px-7 py-2 bg-[#169976] cursor-pointer rounded-lg w-full text-white font-semibold text-2xl"
+        >
+          {isActive ? "Pause" : "Start"}
+        </button>
       </div>
     </section>
   );
--- server/controllers/hoursController.js
+++ server/controllers/hoursController.js
@@ -1,6 +1,7 @@
 import Hours from "../models/hoursModel.js";
 import User from "../models/userModel.js";
 
+// Creating Hours
 export const createHours = async (req, res) => {
   const { time, hoursTarget } = req.body;
   try {
@@ -17,6 +18,25 @@ export const createHours = async (req, res) => {
     await newHours.save();
 
     res.status(201).json(newHours);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Updating Hours
+export const updateHours = async (req, res) => {
+  const { id } = req.params;
+  const { time } = req.body;
+
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(401).json({ message: "User not authenticated." });
+    }
+
+    const hours = await Hours.findByIdAndUpdate(id, { time }, { new: true });
+
+    res.status(200).json(hours);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
