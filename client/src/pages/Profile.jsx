67a150b29d080e96a40ec8eb281a26163921f4c3
Francis <kikobilas123@gmail.com>
2025-04-18 16:54:53 +0800

    Added: A profile page that a user can update (No Functionality yet)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import History from "../components/History";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const { user } = useSelector((state) => state.auth);
+  const [formData, setFormData] = useState({
+    username: user?.username || "",
+    email: user?.email,
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
   return (
-    <section>
-      <div></div>
+    <section className="h-[100svh] w-full grid place-item-center">
+      <div className="flex w-full">
+        <History />
+
+        <form className=" w-full h-full grid place-items-center ">
+          <div className="profile-container mx-auto bg-[#169976] p-3 rounded-2xl">
+            <header className="mb-10">
+              <h1 className="text-4xl font-bold uppercase text-center text-white">
+                Profile
+              </h1>
+            </header>
+
+            <main className="w-full flex flex-col gap-5">
+              <div className="w-full flex flex-col">
+                <span className="text-white text-2xl">Name</span>
+                <input
+                  type="text"
+                  name="username"
+                  id=""
+                  value={formData.username}
+                  onChange={handleChange}
+                  className="input-style rounded-4xl"
+                />
+              </div>
+
+              <div className="w-full flex flex-col">
+                <span className="text-white text-2xl">Email</span>
+                <input
+                  disabled
+                  type="email"
+                  name="email"
+                  id=""
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="input-style rounded-4xl hover:cursor-not-allowed"
+                />
+              </div>
+
+              <div className="w-full flex flex-col">
+                <span className="text-white text-2xl">Password</span>
+                <input
+                  type="password"
+                  name="password"
+                  id=""
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="input-style rounded-4xl"
+                />
+              </div>
+
+              <button className="p-2 bg-[#222] text-white text-xl rounded-3xl cursor-pointer hover:bg-green-700">
+                Update Profile
+              </button>
+
+              <p className="text-red-600 cursor-pointer underline text-end">
+                Delete Account
+              </p>
+            </main>
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
