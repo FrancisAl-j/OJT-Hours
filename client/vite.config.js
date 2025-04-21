5f5060be9578c95e15feb5f2c5856ccfec608cd5
Francis <kikobilas123@gmail.com>
2025-04-21 22:29:39 +0800

    Update some things

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/OJT-Hours",
+  base: "/",
 });
 
 //import.meta.env.VITE_BASE_PATH ||
